92a25f9de747cf67e81ea923a1845cb49222ce80
a22marca <128985931+a22marca@users.noreply.github.com>
2023-04-20 09:01:20 +0200

    Lagt till onClick metod som startar bläddring av ViewFlipper

--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -3,12 +3,25 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ViewFlipper;
 
 public class SecondActivity extends AppCompatActivity {
 
+    private ViewFlipper flipper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        flipper = findViewById(R.id.flipper);
+        flipper.setFlipInterval(1000);
+
+        flipper.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                flipper.startFlipping();
+            }
+        });
     }
 }
