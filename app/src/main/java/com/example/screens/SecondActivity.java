af8177404f1a0a0f31ba4bcfda6bab86c38b8f55
a22marca <128985931+a22marca@users.noreply.github.com>
2023-04-20 12:08:55 +0200

    SecondActivity visar emojis baserat på vald kategori i spinner i MainActivity(skickas med i bundle)

--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -24,7 +24,33 @@ public class SecondActivity extends AppCompatActivity {
             selectedCategory = extras.getString("category");
         }
 
+        // get textViews from ViewFlipper
+        flipper = findViewById(R.id.flipper);
+        TextView[] views = new TextView[flipper.getChildCount()];
+        for (int i = 0; i < flipper.getChildCount(); i++) {
+            views[i] = (TextView) flipper.getChildAt(i);
+        }
+
+        // Select emojis based on the selected category
+        Resources resources = getResources();
+        String[] emojis = new String[3];
+        if (selectedCategory.equals("Cats")) {
+            emojis = resources.getStringArray(R.array.cats);
+        } else if (selectedCategory.equals("Dogs")) {
+            emojis = resources.getStringArray(R.array.dogs);
+        } else if (selectedCategory.equals("Party")) {
+            emojis = resources.getStringArray(R.array.party);
+        }
+
+        // setText for the TextViews in the ViewFlipper
+        for (int i = 0; i < views.length; i++){
+            views[i].setText(emojis[i]);
+        }
+
+        // Flipping speed
         flipper.setFlipInterval(1000);
+
+        // Method to start flipping the View Flipper
         flipper.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
